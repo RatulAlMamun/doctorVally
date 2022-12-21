793381113cf4e9550e5f7772d878af1fd52da0b7
AfiaAkterMunni <afiamunni03@gmail.com>
2022-12-21 23:02:57 +0600

    implement blog create API.

---
+++ app/Http/Controllers/BlogController.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Http\Requests\BlogRequest;
+use App\Models\Blog;
+use App\Models\User;
+
+class BlogController extends Controller
+{
+    public function create(BlogRequest $request)
+    {
+        $data = [
+            'title' => $request->input('title'),
+            'description' => $request->input('description'),
+            'user_id' => auth()->id()
+        ];
+        if($request->hasFile('thumbnail'))
+        {
+            $thumbnail = $request->file('thumbnail');
+            $newThumbnailName = time().'.'.$thumbnail->getClientOriginalExtension();
+            $path = public_path('/uploads/blogs');
+            $thumbnail->move($path, $newThumbnailName);
+            $data['thumbnail'] = $newThumbnailName;
+        }
+        $blog = Blog::create($data);
+        return response()->json([
+            'error' => 'false',
+            'message' => 'Blog created successfully!!',
+            'data' => $blog
+        ], 201);
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\AuthController;
+use App\Http\Controllers\BlogController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -23,6 +24,7 @@ Route::post('/register',[AuthController::class,'register']);
 Route::post('/login',[AuthController::class,'login']);
 
 Route::middleware('auth:api')->group(function(){
-    Route::get('/me',[AuthController::class,'me']);
+    Route::get('/me', [AuthController::class,'me']);
+    Route::post('/blog/create', [BlogController::class, 'create']);
 });
 
